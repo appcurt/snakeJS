c090593a85286f632fe3976ac66b2141aa971d3c
curtathompson <cthompson@pasedfoundation.org>
2014-12-10 19:42:51 -0800

    added draw snake, snake now moves left to right

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,4 +1,6 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
@@ -16,13 +18,50 @@ function gameInitialize() {
 }
 
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
 }
 
 function gameDraw() {
     context.fillStyle = "rgb(100,190,200)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+    
+    snakeDraw();
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for(var i = snakeLength - 1; i >= 0; i--) {
+        snake.push( {
+            x: i,
+            y: 0
+        });
+    }
+}
+
+function snakeDraw() {
+    for(var i = 0; i < snake.length; i++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[i].x * snakeSize, snake[i].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
 }
 
 gameInitialize();
-gameDraw();
+snakeInitialize();
+setInterval(gameLoop, 1000/30);
