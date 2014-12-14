39aee02358743855a34b4329212ca0222886dd47
ct <ct@vash>
2014-12-13 21:28:32 -0800

    fixed food position, added food collision code

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -90,6 +90,8 @@ function snakeUpdate() {
         snakeHeadX++;
     }
     
+    checkFoodCollisions(snakeHeadX, snakeHeadY);
+    
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
@@ -111,16 +113,21 @@ function foodInitialize() {
 
 function foodDraw() {
     context.fillStyle = "red";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
+
+/*  ==============================================
+ *                  Input Functions
+ *  ==============================================
+ */ 
 
 function keyboardHandler(event) {
     console.log(event);
@@ -139,14 +146,19 @@ function keyboardHandler(event) {
     }
 }
 
+function checkFoodCollisions(snakeHeadX, snakeHeadY) {
+    if (snakeHeadX == food.x && snakeHeadY == food.y) {
+        console.log("food collision");
+    }
+}
 
 /*
- *  -----------------------------------------------------------
+ * -----------------------------------------------------------
  * |                     THE GAME                            |
  * -----------------------------------------------------------
  */
 gameInitialize();
 snakeInitialize();
 foodInitialize();
-setInterval(gameLoop, 1000/60);
+setInterval(gameLoop, 1000/25);
 
